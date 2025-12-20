9336530e45890a30c14f4adb520169f51f8c9c04
Advait Contractor <advait@advaitconty.com>
2025-12-20 17:31:26 +0800

    Initial Commit

---
+++ Trial Manager/Trial_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trial_ManagerApp.swift
+//  Trial Manager
+//
+//  Created by Milind Contractor on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Trial_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
